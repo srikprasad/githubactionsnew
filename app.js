237062b9f17277edf502c0889770130954c1ecb0
H.K.Srilakshmi <srikprasad@msrit.edu>
2023-08-22 10:16:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 
 app.get('/',(req,res) => {
     res.status(200);
-    res.send("Hello World Git Hub Actions!!");
+    res.send("Hello World Nodejs!!");
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT} `));
